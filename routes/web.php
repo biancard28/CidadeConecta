afd4eaa148570dc81771d922017b8465982c673c
Vitor <vitorhugotavares765@gmail.com>
2026-02-24 21:07:29 -0300

    conserto

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 
 Route::get('/', function () {
     return view('welcome');
 });
-=======
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CidadeController;
 
@@ -15,7 +13,6 @@ Route::get('/cidade', [CidadeController::class,'index'])->name('cidade.index');
 Route::get('/cidade/create', [CidadeController::class,'create'])->name('cidade.create');
 Route::post('/cidade', [CidadeController::class,'store'])->name('cidade.store');
 Route::resource('users', UserController::class);
->>>>>>> f7be7f73ad3c9b4ffe25118100199a942a0c093b
 
 Route::get('/dashboard', function () {
     return view('dashboard');
